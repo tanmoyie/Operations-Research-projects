b5dd5388988a4b361edb6774b00dc9184229d13c
tanmoyie <tanmoyie@dal.ca>
2024-07-10 20:37:53 -0600

    .

--- travelling-salesman-problem/tsp_model.py
+++ travelling-salesman-problem/tsp_model.py
@@ -10,7 +10,7 @@ class TravelingSalesmanProblem:
         self.model = self.create_model()
 
     def generate_distance_matrix(self):
-        np.random.seed(0)
+        np.random.seed(20)
         distance_matrix = np.random.randint(10, 100, size=(self.num_cities, self.num_cities))
         np.fill_diagonal(distance_matrix, 0) # since we created random distances, diagonals also contain values,
         # which is supposed to be zero.
@@ -19,13 +19,15 @@ class TravelingSalesmanProblem:
 
     def create_model(self):
         model = pyo.ConcreteModel()
-        model.N = pyo.RangeSet(self.num_cities)
+        model.N = pyo.RangeSet(self.num_cities)  # pyo.Set(initialize=range(1, self.num_cities+1)) #
         model.distance = pyo.Param(model.N, model.N, initialize=lambda model, i, j: self.distance_matrix[i - 1, j - 1])
-        model.x = pyo.Var(model.N, model.N, domain=pyo.Binary)
+        model.x = pyo.Var(model.N, model.N, within=pyo.Binary)
         model.u = pyo.Var(model.N, within=pyo.NonNegativeReals)
 
         def obj_rule(model):
-            return pyo.summation(model.distance, model.x)
+            return sum(model.distance[i, j] * model.x[i, j] for i in model.N for j in model.N)
+
+            # (pyo.summation(model.distance, model.x))
 
         model.obj = pyo.Objective(rule=obj_rule, sense=pyo.minimize)
 
@@ -42,25 +44,32 @@ class TravelingSalesmanProblem:
         def sub_tour_elimination_rule(model, i, j):
             if i != j and i != 1 and j != 1:
                 return model.u[i] - model.u[j] + self.num_cities * model.x[i, j] <= self.num_cities - 1
-            return pyo.Constraint.Skip
+            else:
+                return pyo.Constraint.Skip
 
-        # model.sub_tour_elim = pyo.Constraint(model.N, model.N, rule=sub_tour_elimination_rule)
-        print(model)
+        model.sub_tour_elim = pyo.Constraint(model.N, model.N, rule=sub_tour_elimination_rule)
+
         return model
 
     def solve(self):
         solver = SolverFactory('glpk')
-        result = solver.solve(self.model)
+        result = solver.solve(self.model, tee=True)
         # result = solver.solve(self.model).write()
 
-        self.model.pprint()
-        print(f'{result.Solver.status=}, {self.model.cost_function()}')
+        # self.model.pprint()
+        print('result.Solver.status', f'{result.Solver.status=}')  #, {self.model.cost_function()}
 
         route = []
-        for i in range(1, self.num_cities + 1):
-            for j in range(1, self.num_cities + 1):
-                if pyo.value(self.model.x[i, j]) > 0.5:
-                    route.append((i, j))
+        # for i in range(1, self.num_cities + 1):
+        #    for j in range(1, self.num_cities + 1):
+                # if pyo.value(self.model.x[i, j]) == 1.0:
+                #     route.append((i, j))
+
+        l = list(self.model.x.keys())
+        for i in l:
+            if self.model.x[i]() == 1.0:
+                print(i, '--', self.model.x[i]())
+                route.append(i)
 
         return route
 
